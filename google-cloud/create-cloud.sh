30bb25e220b95e5524b2d32d4f8369729736d63a
siim <siim.gitclient@talvik.eu>
2017-06-09 15:25:24 +0300

    changed startup.sh include url.

--- google-cloud/create-cloud.sh
+++ google-cloud/create-cloud.sh
@@ -1,3 +1,3 @@
 gcloud deployment-manager deployments create docker \
---config https://docker-for-gcp-templates.storage.googleapis.com/v8/Docker.jinja \
+--config https://raw.githubusercontent.com/simpss/docker-course/master/google-cloud/Docker.jinja \
 --properties managerCount:3,workerCount:1,zone:europe-west1-d
